5472c47cae820928ce5e2d4462514828622238e0
Marek Mościchowski <marek.moscichowski@miquido.com>
2021-04-22 13:56:30 +0200

    x

--- outputs.tf
+++ outputs.tf
@@ -8,6 +8,11 @@ output "target_group_arn" {
   value       = join("", data.aws_lb_target_group.default.*.arn)
 }
 
+output "code_deploy_group_arn" {
+  description = "ALB Target Group ARN"
+  value       = local.code_deploy_group_arn
+}
+
 output "target_group_arn_suffix" {
   description = "ALB Target Group ARN suffix"
   value       = join("", data.aws_lb_target_group.default.*.arn_suffix)
